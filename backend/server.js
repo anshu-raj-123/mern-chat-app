a7c9b4fa1e0f8ddc67889e2cb6c0354b59eb9736
simplelearn github <ranshu233@gmail.com>
2024-07-02 10:42:39 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,10 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-dotenv.config();
+// dotenv.config();
 
 const __dirname = path.resolve();
+dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 const PORT = process.env.PORT || 5000;
 
@@ -24,7 +25,6 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
