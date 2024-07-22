6e9a1c67585364514f54f0e524cd122b27ececde
simplelearn github <ranshu233@gmail.com>
2024-07-22 23:23:20 +0530

    changes

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -6,31 +6,27 @@ const useLogout = () => {
 	const [loading, setLoading] = useState(false);
 	const { setAuthUser } = useAuthContext();
 
-	const logout = () => {
+	const logout = async () => {
 		setLoading(true);
-
-		fetch("/api/auth/logout", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				if (data.error) {
-					throw new Error(data.error);
-				}
-
-				localStorage.removeItem("chat-user");
-				setAuthUser(null);
-			})
-			.catch((error) => {
-				toast.error(error.message);
-			})
-			.finally(() => {
-				setLoading(false);
+		try {
+			const res = await fetch("/api/auth/logout", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
 			});
+			const data = await res.json();
+			if (data.error) {
+				throw new Error(data.error);
+			}
+
+			localStorage.removeItem("chat-user");
+			setAuthUser(null);
+		} catch (error) {
+			toast.error(error.message);
+		} finally {
+			setLoading(false);
+		}
 	};
 
 	return { loading, logout };
 };
-
 export default useLogout;
