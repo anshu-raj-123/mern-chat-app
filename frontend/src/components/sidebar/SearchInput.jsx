707d5fa087ae0b3efce9e77065859563e1554a65
simplelearn github <ranshu233@gmail.com>
2024-08-04 15:02:30 +0530

    changes

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -5,32 +5,37 @@ import useGetConversations from "../../hooks/useGetConversations";
 import toast from "react-hot-toast";
 
 const SearchInput = () => {
-	const [search, setSearch] = useState("");
+	const [query, setQuery] = useState("");
 	const { setSelectedConversation } = useConversation();
 	const { conversations } = useGetConversations();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (!search) return;
-		if (search.length < 3) {
+	const handleSearch = (event) => {
+		event.preventDefault();
+		if (!query) return;
+		if (query.length < 3) {
 			return toast.error("Search term must be at least 3 characters long");
 		}
 
-		const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+		const foundConversation = conversations.find((conv) =>
+			conv.fullName.toLowerCase().includes(query.toLowerCase())
+		);
 
-		if (conversation) {
-			setSelectedConversation(conversation);
-			setSearch("");
-		} else toast.error("No such user found!");
+		if (foundConversation) {
+			setSelectedConversation(foundConversation);
+			setQuery("");
+		} else {
+			toast.error("No such user found!");
+		}
 	};
+
 	return (
-		<form onSubmit={handleSubmit} className='flex items-center gap-2'>
+		<form onSubmit={handleSearch} className='flex items-center gap-2'>
 			<input
 				type='text'
 				placeholder='Search…'
 				className='input input-bordered rounded-full'
-				value={search}
-				onChange={(e) => setSearch(e.target.value)}
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
 			/>
 			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
 				<IoSearchSharp className='w-6 h-6 outline-none' />
@@ -38,5 +43,5 @@ const SearchInput = () => {
 		</form>
 	);
 };
-export default SearchInput;
 
+export default SearchInput;
